a741e565a71f82b3531402cdda97eec9535fcd68
mepeter <michaelpeterson2@gmail.com>
2019-02-22 14:28:17 -0700

    Put Tabs on All

--- src/components/Bio/Align.js
+++ src/components/Bio/Align.js
@@ -12,38 +12,42 @@ class Align extends React.Component
   {
     return (
     <div className='content_footer'>
-    <Tabs value='alignment'/>
-      <div className='lr'>
+    <div className='background'>
+      <Tabs value='alignment'/>
+      <div className='m'>
+        <div className='lr'>
 
-        <div className="left">
-          <h3>Select Alignment:</h3>
-          <table id="simple-board">
-             <tbody>
-               <tr id="row0">
-                 <td id="cell0-0"><button>Lawful Good</button></td>
-                 <td id="cell0-1"><button>Neutral Good</button></td>
-                 <td id="cell0-2"><button>Chaotic Good</button></td>
-               </tr>
-               <tr id="row1">
-                 <td id="cell1-0"><button>Lawful Neutral</button></td>
-                 <td id="cell1-1"><button>True Neutral</button></td>
-                 <td id="cell1-2"><button>Chaotic Neutral</button></td>
-               </tr>
-               <tr id="row2">
-                 <td id="cell2-0"><button>Lawful Evil</button></td>
-                 <td id="cell2-1"><button>Neutral Evil</button></td>
-                 <td id="cell2-2"><button>Chaotic Evil</button></td>
-               </tr>
-             </tbody>
-           </table>
+          <div className="left">
+            <h3>Select Alignment:</h3>
+            <table id="simple-board">
+               <tbody>
+                 <tr id="row0">
+                   <td id="cell0-0"><button className='button_class'>Lawful Good</button></td>
+                   <td id="cell0-1"><button className='button_class'>Neutral Good</button></td>
+                   <td id="cell0-2"><button className='button_class'>Chaotic Good</button></td>
+                 </tr>
+                 <tr id="row1">
+                   <td id="cell1-0"><button className='button_class'>Lawful Neutral</button></td>
+                   <td id="cell1-1"><button className='button_class'>True Neutral</button></td>
+                   <td id="cell1-2"><button className='button_class'>Chaotic Neutral</button></td>
+                 </tr>
+                 <tr id="row2">
+                   <td id="cell2-0"><button className='button_class'>Lawful Evil</button></td>
+                   <td id="cell2-1"><button className='button_class'>Neutral Evil</button></td>
+                   <td id="cell2-2"><button className='button_class'>Chaotic Evil</button></td>
+                 </tr>
+               </tbody>
+             </table>
+          </div>
+
+          <div className='right'>
+            <h3>Description</h3>
+            <p>Lots of text</p>
+          </div>
+
+
         </div>
-
-        <div className='right'>
-          <h3>Description</h3>
-          <p>Lots of text</p>
         </div>
-
-
       </div>
       <Footer prev="/bio" next='/equipment' />
     </div>
--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -14,46 +14,49 @@ class Bio extends React.Component
   {
     return (
     <div className='content_footer'>
-    <Tabs value='bio'/>
-      <div className='lr'>
-          <div className='left'>
-              <div className='ud'>
-                      <h1>Bio</h1>
-                      <h3>Select Gender</h3>
-                        <div className='lr'>
-                          <button>Male</button>
-                          <button>Female</button>
-                        </div>
+      <div className='background'>
+        <Tabs value='bio'/>
+          <div className='m'>
+              <div className='lr'>
+                  <div className='left'>
+                      <div className='ud'>
+                              <h1>Bio</h1>
+                              <h3>Select Gender</h3>
+                                <div className='lr'>
+                                  <button>Male</button>
+                                  <button>Female</button>
+                                </div>
 
-                      <h3>Set Height </h3>
-                        <div className='lr'>
-                              <input type="text" name="name" />
-                              ft
-                              <input type="text" name="name" />
-                              in
-                          <button>Random</button>
-                        </div>
+                              <h3>Set Height </h3>
+                                <div className='lr'>
+                                      <input type="text" name="name" />
+                                      ft
+                                      <input type="text" name="name" />
+                                      in
+                                  <button>Random</button>
+                                </div>
 
-                      <h3>Set Weight </h3>
-                        <div className='lr'>
-                          <form>
-                            <label>
-                              <input type="text" name="name" />
-                              lbs
-                            </label>
-                          </form>
-                          <button>Random</button>
+                              <h3>Set Weight </h3>
+                                <div className='lr'>
+                                  <form>
+                                    <label>
+                                      <input type="text" name="name" />
+                                      lbs
+                                    </label>
+                                  </form>
+                                  <button>Random</button>
+                                </div>
                         </div>
-                </div>
+                  </div>
+
+                  <div className='right'>
+                      <h3>Biography (optional)</h3>
+                      <input type="text" name="name" />
+                  </div>
+              </div>
           </div>
-
-          <div className='right'>
-              <h3>Biography (optional)</h3>
-              <input type="text" name="name" />
-          </div>
-      </div>
-
-      <Footer prev="/stats" next='/alignment' />
+     </div>
+          <Footer prev="/stats" next='/alignment' />
     </div>
     );
   }
