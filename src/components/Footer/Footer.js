5704c2fbd228fbef311454001c7dfc0b075936b5
lkjames <thelancejames@gmail.com>
2019-02-22 22:57:39 -0700

    footer buttons hover

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -20,14 +20,18 @@ class Footer extends React.Component
   {
     return (
       <div className='foot'>
-        <Link to={this.prev}><button className='b_prev button_foot_border button_prev_border'> </button></Link>
-        <Link to={this.prev}><button className='b_prev button_foot button_prev'> </button></Link>
-        <Link to={this.prev}><button className='b_prev button_foot_front_edge_border button_prev_front_edge_border'> </button></Link>
-        <Link to={this.prev}><button className='b_prev button_foot_front_edge button_prev_front_edge'> </button></Link>
-        <Link to={this.next}><button className='b_next button_foot_border button_next_border'> </button></Link>
-        <Link to={this.next}><button className='b_next button_foot button_next'> </button></Link>
-        <Link to={this.next}><button className='b_next button_foot_front_edge_border button_next_front_edge_border'> </button></Link>
-        <Link to={this.next}><button className='b_next button_foot_front_edge button_next_front_edge'> </button></Link>
+        <div className='prev'>
+          <Link to={this.prev}><button className='b_prev button_foot_border button_prev_border'> </button></Link>
+          <Link to={this.prev}><button className='b_prev button_foot button_prev'> </button></Link>
+          <Link to={this.prev}><button className='b_prev button_foot_front_edge_border button_prev_front_edge_border'> </button></Link>
+          <Link to={this.prev}><button className='b_prev button_foot_front_edge button_prev_front_edge'> </button></Link>
+        </div>
+        <div className='next'>
+          <Link to={this.next}><button className='b_next button_foot_border button_next_border'> </button></Link>
+          <Link to={this.next}><button className='b_next button_foot button_next'> </button></Link>
+          <Link to={this.next}><button className='b_next button_foot_front_edge_border button_next_front_edge_border'> </button></Link>
+          <Link to={this.next}><button className='b_next button_foot_front_edge button_next_front_edge'> </button></Link>
+        </div>
       </div>
     );
   }
