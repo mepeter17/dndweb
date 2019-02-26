002fb3777e29718ed52720a16b15d3b202ea1c37
mepeter17 <michaelpeterson2@gmail.com>
2019-02-26 05:22:20 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dndweb/precache-manifest.a208916e58d58fa25247641ab467868f.js"
+  "/ddd/precache-manifest.496e4b269fe4e798a2264fde93201fb6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dndweb/index.html", {
+workbox.routing.registerNavigationRoute("/ddd/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
